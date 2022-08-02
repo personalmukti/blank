7d17fcada1aea3a45a131f90d481c26cca4f0c6d
Mukti <mukti.devmail@gmail.com>
2022-08-02 15:42:55 +0700

    Fix minor Layouting

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,7 +1,8 @@
+<li class="nav-header">Website Management</li>
 <li class="nav-item">
     <a href="{{ route('websettings.index') }}"
        class="nav-link {{ Request::is('websettings*') ? 'active' : '' }}">
-        <p>@lang('models/websettings.plural')</p>
+        <p>@lang('models/websettings.singular')</p>
     </a>
 </li>
 
